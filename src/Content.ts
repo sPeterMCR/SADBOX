9486ffece03bf72ab3476adc5d2456ce50b9bd16
sPeterMCR <schmeidlpetersp@gmail.com>
2020-02-10 19:37:15 +0100

    commit

--- src/Content.ts
+++ src/Content.ts
@@ -1,6 +1,10 @@
-﻿import fs from "fs";
+﻿/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prefer-const */
+/* eslint-disable @typescript-eslint/no-inferrable-types */
+/* eslint-disable no-var */
+import fs, { write } from "fs";
 import http from "http";
-import url from "url";
 
 interface InputInterface {
     name: string;
@@ -22,38 +26,40 @@ export default class Content {
         res.write("<head>");
         res.write("<style>input, pre {font-family:monospace; font-size:1em; font-weight:bold;}</style>");
         res.write("<meta name='viewport' content='width=device-width, initial-scale=1.0'>");
-        res.write("<title>Jedlik Ts Template</title>");
+        res.write("<title>SandBox</title>");
         res.write("</head>");
         res.write("<body><form><pre>");
 
         // Kezd a kódolást innen -->
 
-        res.write("Egyszerű Hello World!\n");
+        var age: number = 32; // number variable
+        var name: string = "John"; // string variable
+        var isUpdated: boolean = true; // Boolean variable
+        let autok: Array<string>; //array						
+        autok = ["Lambo", "Ford", "Chevrolet"];				
+        enum sportcars {									
+            Porsche = 8,									
+            Rx7 = 4,									
+            BMW = 7,								
+            Ferrari = 9									
+        }
+        let Kozso: string | number; //union						
+        Kozso = 365;									
+        Kozso = "CBA";								
+        let kiiras: any = "Szia anya!";							
+        kiiras = 25;									
+        function kozsowriter(): void {							
+            res.write("55535553655");							
+        }
+        let teloszam: void = kozsowriter();						
 
-        // Tetszőleges html teg-ek és attribútumok beépítése:
-        res.write("<span style='color: blue;'><i>Színes és dőlt Hello World!'</i></span>\n");
+        let biblia: string = "Jézus";							
+        const biblia2: string = "Krisztus";							
 
-        // Egyszerű input JSON állományból:
-        const input: InputInterface = JSON.parse(fs.readFileSync("input.json", "utf-8"));
-
-        // String template használata
-        res.write(`1. feladat: ${input.name} kora: ${input.age}\n`);
-
-        // Változó definiálása:
-        let neme = "";
-        neme = input.male ? "férfi" : "nő";
-        res.write(`2. feladat: ${input.name} neme: ${neme}\n`);
-
-        // Input form-al és <input type='number'>-el:
-        // URL paraméter(ek) (itt kor) ellenőrzése,  kiolvasása, alapértelmezett értéke 18:
-        const u = url.parse(req.url as string, true).query;
-        // ha "kor" paraméter nincs megadva vagy "kor" paraméter üres string,
-        // akkor legyen 18 az értéke, egyébként konvertáljuk számra a "kor" paraméter értékét:
-        // let kor: number = u.kor === undefined || u.kor === "" ? 18 : parseInt(u.kor as string);
-        let kor: number = parseInt(u.kor as string);
-        if (isNaN(kor) || kor < 0 || kor > 99) kor = 18; // egy kis ellenőrzés
-        res.write(`3. feladat: Kérem a korod [0-99]: <input type='text' name='kor' value=${kor} style='width:3em;'>\n`);
-        res.write(`4. feladat: Te ${kor} éves vagy!\n`);
+        res.write(` ${age} ${name} ${isUpdated} `);
+        res.write(`${autok}`);
+        res.write(`${sportcars.Porsche}, ${sportcars.Rx7}, ${sportcars.BMW}, ${sportcars.Ferrari} `);
+        res.write(`${Kozso} ${kiiras} `);
 
         // <---- Fejezd be a kódolást
 
